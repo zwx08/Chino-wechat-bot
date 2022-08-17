273dbbea362df749510c4df18359cc53d6f1109c
zwx08 <zwx.08@qq.com>
2022-08-17 19:03:59 +0800

    Update load_plugins.py

--- load_plugins.py
+++ load_plugins.py
@@ -6,7 +6,7 @@ from action_sql import *
 
 cur_path = os.path.dirname(os.path.realpath(__file__))# 当前脚本所在的文件绝对路径
 
-os.putenv("PYTHONPATH", cur_path)# 将当前路径设置为python的临时环境变量，用于命令执行,需要设置是因为项目存在多处相互调用
+os.putenv("PYTHONPATH", cur_path)# 将当前路径设置为python的临时环境变量，用于命令执行,需要设置是因为项目存在多处相互调用(好吧并没有，只是复制下来这段代码但是又懒的删了，也许后面能用到）
 
 def run_case():
     case_path = os.path.join(cur_path, "plugins")# 使用os.path.join拼接地址
